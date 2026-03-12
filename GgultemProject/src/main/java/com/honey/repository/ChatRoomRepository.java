53e44afb434350566bdd814f21103f99b3df7d1d
KTH <sauce0428@gmail.com>
2026-03-12 18:06:36 +0900

    read / register 생성

--- GgultemProject/src/main/java/com/honey/repository/ChatRoomRepository.java
+++ GgultemProject/src/main/java/com/honey/repository/ChatRoomRepository.java
@@ -1,5 +1,9 @@
 package com.honey.repository;
 
-public class ChatRoomRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.honey.domain.ChatRoom;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
 }
